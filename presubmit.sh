0f0060ea4bf1f58dcfddec3f19e008d0b68783da
John Strunk <jstrunk@redhat.com>
2018-05-10 16:16:49 -0400

    Add travis and cleanup
    Signed-off-by: John Strunk <jstrunk@redhat.com>

---
+++ presubmit.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+# vim: set ts=4 sw=4 et :
+
+set -e
+
+if [ -x "$(command -v asciidoctor)" ]; then
+    find . -regextype egrep -iregex '.*\.adoc' -print0 | \
+        xargs -0rt -n1 asciidoctor -o /dev/null -v --failure-level WARN
+else
+    echo "Warning: Not checking *.adoc files... no asciidoctor"
+fi
+
+if [ -x "$(command -v mdl)" ]; then
+    find . -regextype egrep -iregex '.*\.md' -print0 | \
+        xargs -0rt mdl
+else
+    echo "Warning: Not checking *.md files... no mdl (markdownlint)"
+fi
+
+if [ -x "$(command -v shellcheck)" ]; then
+    find . -regextype egrep -iregex '.*\.(ba)?sh' -print0 | \
+        xargs -0rt shellcheck
+else
+    echo "Warning: Not checking .sh/.bash files... no shellcheck"
+fi
+
+if [ -x "$(command -v yamllint)" ]; then
+    find . -regextype egrep -iregex '.*\.ya?ml' -print0 | \
+        xargs -0rt yamllint
+else
+    echo "Warning: Not checking yaml files... no yamllint"
+fi
+
+echo "ALL OK."
