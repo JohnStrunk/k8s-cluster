0e9e84fb814506dbdcd63ba7e47fabb164434f28
John Strunk <jstrunk@redhat.com>
2018-07-23 16:53:52 -0400

    Update pre-submit script to better show where errors are
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- presubmit.sh
+++ presubmit.sh
@@ -1,34 +1,50 @@
 #! /bin/bash
 # vim: set ts=4 sw=4 et :
 
+# Usage: pre-commit.sh [--require-all]
+#   --require-all  Fail instead of warn if a checker is not found
+
 set -e
 
-if [ -x "$(command -v asciidoctor)" ]; then
-    find . -regextype egrep -iregex '.*\.adoc' -print0 | \
-        xargs -0rt -n1 asciidoctor -o /dev/null -v --failure-level WARN
-else
-    echo "Warning: Not checking *.adoc files... no asciidoctor"
+# Run checks from root of the repo
+scriptdir="$(dirname "$(realpath "$0")")"
+cd "$scriptdir"
+
+# run_check <file_regex> <checker_exe> [optional args to checker...]
+function run_check() {
+    regex="$1"
+    shift
+    exe="$1"
+    shift
+
+    if [ -x "$(command -v "$exe")" ]; then
+        find . -regextype egrep -iregex "$regex" -print0 | \
+            xargs -0rt -n1 "$exe" "$@"
+    elif [ "$all_required" -eq 0 ]; then
+        echo "Warning: $exe not found... skipping some tests."
+    else
+        echo "FAILED: All checks required, but $exe not found!"
+        exit 1
+    fi
+}
+
+all_required=0
+if [ "x$1" == "x--require-all" ]; then
+    all_required=1
 fi
 
-if [ -x "$(command -v mdl)" ]; then
-    find . -regextype egrep -iregex '.*\.md' -print0 | \
-        xargs -0rt mdl
-else
-    echo "Warning: Not checking *.md files... no mdl (markdownlint)"
-fi
+# Install via: gem install asciidoctor
+run_check '.*\.adoc' asciidoctor -o /dev/null -v --failure-level WARN
 
-if [ -x "$(command -v shellcheck)" ]; then
-    find . -regextype egrep -iregex '.*\.(ba)?sh' -print0 | \
-        xargs -0rt shellcheck
-else
-    echo "Warning: Not checking .sh/.bash files... no shellcheck"
-fi
+# markdownlint: https://github.com/markdownlint/markdownlint
+# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
+# Install via: gem install mdl
+run_check '.*\.md' mdl
 
-if [ -x "$(command -v yamllint)" ]; then
-    find . -regextype egrep -iregex '.*\.ya?ml' -print0 | \
-        xargs -0rt yamllint -s
-else
-    echo "Warning: Not checking yaml files... no yamllint"
-fi
+# Install via: dnf install shellcheck
+run_check '.*\.(ba)?sh' shellcheck
+
+# Install via: pip install yamllint
+run_check '.*\.ya?ml' yamllint -s
 
 echo "ALL OK."
